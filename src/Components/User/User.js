8420d8e731cb7ba1540b18b9bc4b3662de180ad1
unknown <rafahmp123@gmail.com>
2022-07-04 20:09:27 -0300

    Criando/Estilizando header da pagina de conta

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import { Routes, Route } from 'react-router-dom';
+import UserStats from './UserStats';
 
 const User = () => {
-  return <section>User</section>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
