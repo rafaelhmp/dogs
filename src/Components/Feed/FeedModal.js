a8001a0f522e2e1fe277ee3329105d1be95366de
unknown <rafahmp123@gmail.com>
2022-07-05 17:09:30 -0300

    Criando estrutura/estilo inicial do modal de foto

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from './FeedModal.module.css';
+import UseFetch from '../../Hooks/UseFetch';
+import { PHOTO_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = UseFetch();
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
