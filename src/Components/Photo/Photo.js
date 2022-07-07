efff1bd21c43336819359af755e423ba526be9c3
unknown <rafahmp123@gmail.com>
2022-07-07 20:35:11 -0300

    Adicionando scroll infinito/pagina da foto/pagina do usuario

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import UseFetch from '../../Hooks/UseFetch';
+import { PHOTO_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
 const Photo = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const { data, loading, error, request } = UseFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent data={data} single={true} />
+      </section>
+    );
+  else return null;
 };
 
 export default Photo;
