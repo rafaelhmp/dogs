c6d64bac885b9b1fd0381ba6de08d3ef0004aeb1
unknown <rafahmp123@gmail.com>
2022-07-11 17:06:03 -0300

    Adicionando biblioteca Victory para pagina de estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import UseFetch from '../../Hooks/UseFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatisticas" />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = UseFetch();
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatisticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
